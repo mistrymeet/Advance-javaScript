ce1d10f65df96fecac92472a6e7681d7d71aaa26
mistrymeet <meetmistry88@gmail.com>
2023-07-01 16:52:20 +0530

    task update

--- AJS task/array/city.js
+++ AJS task/array/city.js
@@ -24,3 +24,50 @@ function cityState(state){
 
 let storeFilterState = cityState("Uttar Pradesh")
 console.log("🚀 ~ file: city.js:13 ~ storeFilterState:", storeFilterState)
+
+// 6 city population
+function cityPopulation(population){
+    let filterPopulation = data.filter((x)=>{
+        return x.population === population
+    })
+    let populationCityName = filterPopulation.filter((x)=>{
+        return x.city
+    })
+    let newArr = []
+    for (let i = 0; i < populationCityName.length; i++) {
+        const element = populationCityName[i].city;
+        newArr.push(element)
+    }
+    return newArr
+
+}
+
+let storePopulation = cityPopulation('1131160')
+console.log("======================================", storePopulation)
+
+//3 sum population of city by state name
+function sumCityPopulation(state){
+    let filterState = data.filter((x)=>{
+        return x.state_name === state
+    })
+
+    newArr = []
+    for (let i = 0; i < filterState.length; i++) {
+       let element = filterState[i].population;
+       newArr.push(element)
+    }
+    
+    let element = ''
+    // for (let y = 0; y < newArr.length; y++) {
+    //      element += newArr[y];
+    // }
+    for (const iterator of newArr) {
+        element += iterator
+    }
+    return element
+   
+}
+
+let storeCityPopulation = sumCityPopulation("Uttar Pradesh")
+console.log("sum of population ----------->>", storeCityPopulation)
+
